f977a72c9910c9bd5c559c3f005a0b505ca9e172
Andrew Mondor <mondor.andrew@gmail.com>
2021-04-15 02:08:46 +0200

    data components done
    Signed-off-by: Andrew Mondor <mondor.andrew@gmail.com>

--- src/containers/Global/Global.js
+++ src/containers/Global/Global.js
@@ -1,5 +1,13 @@
 import { Component } from "react"
 import './Global.css'
+
+//import Components
+import worldSats from '../../components/WorldStats/WordlStats'
+import WorldStats from "../../components/WorldStats/WordlStats"
+
+//9:10 
+//Implement the API now 
+
 export default class Global extends Component {
     render () {
         return (
@@ -7,10 +15,25 @@ export default class Global extends Component {
                 <h1 className="heading">Covid 19 tracker</h1>
                 <p className="description">Let's Check Information about covid-19</p>
                 <div className="world-stats">
-                    {/* Show data */}
+                    <WorldStats
+                    key="1"
+                    about="Total Cases"
+                    total="255,134"
+                    />
+
+                    <WorldStats
+                    key="2"
+                    about="Total Deaths"
+                    total="25,134"
+                    />
+
+                    <WorldStats
+                    key="3"
+                    about="Total Recovered"
+                    total="155,134"
+                    />
                 </div> 
             </div>
         )
     }
-
 }
